c9c5c6b4de1d76371cf1ffd904f8c7c8e5a41532
krrajesh2 <krrajesh2@gmail.com>
2013-02-14 15:14:42 -0500

    first commit

---
+++ Feb14/Feb14AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Feb14AppDelegate.h
+//  Feb14
+//
+//  Created by Rajesh Kandasamy on 2/14/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Feb14AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
